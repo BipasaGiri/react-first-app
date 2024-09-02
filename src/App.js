074edc9b08351eb845aa12c27998f9998b9ca52e
BipasaGiri <bipasagiri0@gmail.com>
2024-09-02 12:30:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Line from './component/Line';
+function App() {
+  return (
+  <>
+  <Line></Line>
+  </>
+  );
+}
+
+export default App;
